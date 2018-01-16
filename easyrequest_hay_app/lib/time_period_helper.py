7a62a95b5de4cde62c332546b24db83c608290f0
birkin <birkin.diana@gmail.com>
2018-01-16 13:06:42 -0500

    updates action-url creation.

--- easyrequest_hay_app/lib/time_period_helper.py
+++ easyrequest_hay_app/lib/time_period_helper.py
@@ -3,6 +3,7 @@
 import json, logging, os, pprint, urllib
 from django.http import HttpResponseBadRequest
 from django.template import loader
+from django.core.urlresolvers import reverse
 from easyrequest_hay_app.lib.pickup_location import PickupLocation
 
 
@@ -20,7 +21,10 @@ class TimePeriodHelper( object ):
     def prepare_context( self, r_session ):
         """ Prepares vars for template.
             Called by views.login() """
-        context = { 'item_title': r_session['item_title'] }
+        context = {
+            'item_title': r_session['item_title'],
+            'action_url': reverse( 'time_period_handler_url' )
+        }
         log.debug( 'context, ```%s```' % context )
         return context
 
