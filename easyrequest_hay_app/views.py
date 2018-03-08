ee5c9e004e0bcffd925de3605ba5a4b9a87638be
birkin <birkin.diana@gmail.com>
2018-03-08 16:58:28 -0500

    implements new confirm view.

--- easyrequest_hay_app/views.py
+++ easyrequest_hay_app/views.py
@@ -82,6 +82,38 @@ def time_period_handler( request ):
     return resp
 
 
+def confirm( request ):
+    """ Triggered by user clicking on an Annex-Hay Josiah `request-access` link.
+        Stores referring url, bib, and item-barcode in session.
+        Presents shib and non-shib proceed buttons. """
+    log.debug( 'starting confirm view' )
+    log.debug( 'request.__dict__, ```%s```' % request.__dict__ )
+    if not validator.validate_source(request) and validator.validate_params(request):
+        resp = validator.prepare_badrequest_response( request )
+    else:
+        sess.initialize_session( request )
+        shortlink = tm_prd_helper.save_data( json.dumps(request.GET, sort_keys=True, indent=2) )
+        context = tm_prd_helper.prepare_context( request.GET, shortlink )
+        resp = render( request, 'easyrequest_hay_app_templates/confirm.html', context )
+    return resp
+
+
+def confirm_handler( request ):
+    """ Handler for confirm `shib=yes/no` selection.
+        If `shib=no`, builds Aeon url and redirects.
+        Otherwise submits request to millennium, builds Aeon url and redirects. """
+    log.debug( 'request.__dict__, ```%s```' % request.__dict__ )
+    aeon_url_bldr = AeonUrlBuilder()
+    item_request = get_object_or_404( ItemRequest, short_url_segment=request.GET.get('shortlink', 'foo') )
+    soon_value = request.GET.get( 'soon', '' ).lower()
+    if soon_value == 'yes':
+        resp = tm_prd_hndler_helper.build_soon_response( request.GET['shortlink'] )
+    elif soon_value == 'no':
+        resp = HttpResponseRedirect( aeon_url_bldr.build_aeon_url(item_request.short_url_segment) )
+    else:
+        resp = HttpResponseRedirect( '%s?message=no time-period information found' % reverse('problem_url') )
+    return resp
+
 def login( request ):
     """ Displays millennium shib and non-shib logins.
         Triggered by time_period_hander() """
