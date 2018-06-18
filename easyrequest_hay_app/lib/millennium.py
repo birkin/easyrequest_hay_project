2a116e1b20cf64aa79df2a7411b6e751f12d675f
birkin <birkin.diana@gmail.com>
2018-06-18 14:10:54 -0400

    refactors hold-data preparation.

--- easyrequest_hay_app/lib/millennium.py
+++ easyrequest_hay_app/lib/millennium.py
@@ -18,21 +18,14 @@ class Millennium( object ):
         self.LOCATION_CODE = settings_app.HAY_LOCATION_CODE
         self.item_bib = ''
         self.item_barcode = ''
+        self.item_id = ''
         self.patron_barcode = ''
         self.patron_login_name = ''
 
-    def get_item_id( self, shortlink ):
-        """ Calls api to get the item-id.
-            Called by views.processor() """
-        self.prep_item_data( shortlink )
-        avail_dct = self.hit_availability_api( self.item_bib )
-        item_id = self.extract_item_id( avail_dct, self.item_barcode )
-        log.debug( 'item_id, `%s`' % item_id )
-        return item_id
 
     def prep_item_data( self, shortlink ):
         """ Preps item-data from item_request.
-            Called by get_item_id() """
+            Called by views.processor() """
         item_request = ItemRequest.objects.get( short_url_segment=shortlink )
         item_dct = json.loads( item_request.full_url_params )
         patron_dct = json.loads( item_request.patron_info )
@@ -40,14 +33,33 @@ class Millennium( object ):
         self.item_barcode = item_dct['item_barcode']
         self.patron_barcode = patron_dct['patron_barcode']
         self.patron_login_name = patron_dct['firstname']
+        self.get_item_id()
         log.debug( 'bib, `%s`; item_barcode, `%s`; patron_barcode, `%s`' % (self.item_bib, self.item_barcode, self.patron_barcode) )
         return
+
+    def get_item_id( self ):
+        """ Calls availability-api to get the item-id.
+            Called by views.processor() """
+        avail_dct = self.hit_availability_api( self.item_bib )
+        self.item_id = self.extract_item_id( avail_dct, self.item_barcode )
+        log.debug( 'item_id, `%s`' % self.item_id )
+        return
+
+    # def get_item_id( self, shortlink ):
+    #     """ Calls availability-api to get the item-id.
+    #         Called by views.processor() """
+    #     self.prep_item_data( shortlink )
+    #     avail_dct = self.hit_availability_api( self.item_bib )
+    #     item_id = self.extract_item_id( avail_dct, self.item_barcode )
+    #     log.debug( 'item_id, `%s`' % item_id )
+    #     return item_id
 
     def hit_availability_api( self, bibnum ):
         """ Returns availability-api dct.
             Called by get_item_id() """
         avail_dct = {}
         availability_api_url = '%s/bib/%s' % ( self.AVAILABILITY_API_URL_ROOT, bibnum )
+        log.debug( 'availability_api_url, ```%s```' % availability_api_url )
         try:
             r = requests.get( availability_api_url )
             avail_dct = r.json()
@@ -70,13 +82,13 @@ class Millennium( object ):
         log.debug( 'item_id, `%s`' % item_id )
         return item_id
 
-    def place_hold( self, item_id ):
+    def place_hold( self ):
         """ Calls lib to place hold.
-            Called by views.processor() """
+            Called by views.processor()
+            Elements needed for hold: user_name, user_barcode, item_bib, item_id, pickup_location """
         status = 'init'
         try:
             jos_sess = IIIAccount( name=self.patron_login_name, barcode=self.patron_barcode )
-            log.debug( 'jos_sess, ```%s```' % jos_sess )
             jos_sess.login()
             log.debug( 'jos_sess, ```%s```' % jos_sess )
             hold = jos_sess.place_hold( bib=self.item_bib, item=item_id, pickup_location=self.LOCATION_CODE )
