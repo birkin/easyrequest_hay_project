2fbbc9bcf0e14dbd283129a9c0d30f99891dedf8
birkin <birkin.diana@gmail.com>
2018-07-03 10:07:38 -0400

    removes commented-out code.

--- easyrequest_hay_app/models.py
+++ easyrequest_hay_app/models.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import datetime, json, logging, os, pprint
-# from django.conf import settings as project_settings
-# from django.core.urlresolvers import reverse
 from django.db import models
-# from django.http import HttpResponseRedirect
 
 log = logging.getLogger(__name__)
 
@@ -14,16 +11,6 @@ class ItemRequest( models.Model ):
         Called by Processor(). """
     item_title = models.CharField( max_length=200, blank=True, null=True, help_text="used by Millennium & Aeon" )
     status = models.CharField( max_length=200, blank=True, null=True )
-    # item_author = models.CharField( max_length=200, blank=True, null=True, help_text="used by Aeon" )
-    # item_bib = models.CharField( max_length=50, blank=True, null=True, help_text="used by Millennium & Aeon" )
-    # item_id = models.CharField( max_length=50, blank=True, null=True, help_text="used by Millennium" )
-    # item_barcode = models.CharField( max_length=50, blank=True, null=True, help_text="used by Millennium" )
-    # item_callnumber = models.CharField( max_length=200, blank=True, null=True, help_text="used by Millennium & Aeon" )
-    # item_publish_info = models.CharField( max_length=200, blank=True, null=True, help_text="used by Aeon" )
-    # item_digital_version_url = models.CharField( max_length=200, blank=True, null=True, help_text="used by Aeon" )
-    # patron_name = models.CharField( max_length=100, blank=True, null=True, help_text="used by Millennium" )  # patron full name
-    # patron_barcode = models.CharField( max_length=50, blank=True, null=True, help_text="used by Millennium" )
-    # patron_email = models.CharField( max_length=100, blank=True, null=True, help_text="used by Millennium" )
     source_url = models.TextField( blank=True, null=True )
     create_datetime = models.DateTimeField( auto_now_add=True, blank=True, null=True )  # blank=True for backward compatibility
     modified_datetime = models.DateTimeField( auto_now=True, blank=True, null=True )  # blank=True for backward compatibility
@@ -42,4 +29,4 @@ class ItemRequest( models.Model ):
         object_dct = lst[0]
         return ItemRequest
 
-    # end class ItemRequest
+    ## end class ItemRequest
