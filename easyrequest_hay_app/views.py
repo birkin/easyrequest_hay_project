2b916f6d8d426d0c18de29728cbc679d648db1c1
birkin <birkin.diana@gmail.com>
2019-09-30 09:04:12 -0400

    removes unused code.

--- easyrequest_hay_app/views.py
+++ easyrequest_hay_app/views.py
@@ -138,26 +138,6 @@ def processor( request ):
     return HttpResponseRedirect( aeon_url )
 
 
-# def processor( request ):
-#     """ Behind-the-scenes url which handles item request...
-#         - Gets item-id.
-#         - Attempts to place hold in Sierra.
-#         - Redirects user to Aeon.
-#         Triggered after a successful shib_login (along with patron-api lookup) """
-#     log.debug( 'starting processor(); request.__dict__, ```%s```' % request.__dict__ )
-#     mill_hlpr = Millennium()
-#     aeon_url_bldr = AeonUrlBuilder()
-#     shortlink = request.GET['shortlink']
-#     log.debug( 'shortlink, `%s`' % shortlink )
-#     mill_hlpr.prep_item_data( shortlink )
-#     if mill_hlpr.item_id:  # if we couldn't get an item-id, we can't place a hold
-#         mill_hlpr.call_place_hold()
-#     emailer.run_send_check( mill_hlpr.item_id, mill_hlpr.hold_status, shortlink )
-#     aeon_url_bldr.make_millennium_note( mill_hlpr.item_id, mill_hlpr.item_barcode, mill_hlpr.patron_barcode, mill_hlpr.hold_status )
-#     aeon_url = aeon_url_bldr.build_aeon_url( shortlink )
-#     return HttpResponseRedirect( aeon_url )
-
-
 def problem( request ):
     """ Displays to user a problem message.
         Could be used by a variety of failure situations.
